7419aec4e53f10aec3def9e534a52d422e6416d0
saifaimizumi <168169720+saifaimizumi@users.noreply.github.com>
2024-11-02 16:45:32 +0700

    Update and rename README.md to loader.lua

---
+++ loader.lua
@@ -0,0 +1,168 @@
+--lyxme Hub
+ 
+_G.reel = true
+
+while _G.reel do wait()
+local args = {
+    [1] = 100,
+    [2] = false
+}
+
+game:GetService("ReplicatedStorage").events.reelfinished:FireServer(unpack(args))
+end
+
+
+
+
+local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
+local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/SaveManager.lua"))()
+local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/InterfaceManager.lua"))()
+
+local Window = Fluent:CreateWindow({
+    Title = "lyxme Hub | [🤿]",
+    SubTitle = "",
+    TabWidth = 160,
+    Size = UDim2.fromOffset(510, 390),
+    Acrylic = false, -- The blur may be detectable, setting this to false disables blur entirely
+    Theme = "Amethyst",
+    MinimizeKey = Enum.KeyCode.LeftControl -- Used when theres no MinimizeKeybind
+})
+
+--Fluent provides Lucide Icons https://lucide.dev/icons/ for the tabs, icons are optional
+local Tabs = {
+    Main = Window:AddTab({ Title = "Main", Icon = "rbxassetid://11433532654" }),
+    Settings = Window:AddTab({ Title = "Settings", Icon = "settings" })
+}
+
+local Options = Fluent.Options
+
+do
+    Fluent:Notify({
+        Title = "Notification",
+        Content = "lyxme running script",
+        SubContent = "", -- Optional
+        Duration = 10 -- Set to nil to make the notification not disappear
+    })
+
+end
+
+
+
+local Toggle = Tabs.Main:AddToggle("MyToggle", {Title = "Auto Sell", Default = false })
+
+    Toggle:OnChanged(function(Value)
+        end)
+
+    Options.MyToggle:SetValue(false)
+
+
+local Toggle = Tabs.Main:AddToggle("MyToggle", {Title = "--", Default = false })
+
+Toggle:OnChanged(function(Value)
+while do wait()
+workspace.world.npcs:FindFirstChild("Marc Merchant").merchant.sellall:InvokeServer()
+end
+
+Options.MyToggle:SetValue(false)
+
+
+
+
+local Toggle = Tabs.Main:AddToggle("MyToggle", {Title = "AntiAFK", Default = false })
+
+    Toggle:OnChanged(function(Value)
+        _G.antiAFK = Value
+
+        while _G.antiAFK do wait(20)
+    
+        game:GetService'VirtualUser':Button1Down(Vector2.new(788, 547))
+        
+    end
+    end)
+
+    Options.MyToggle:SetValue(false)
+
+
+
+
+
+    Tabs.Settings:AddButton({
+        Title = "rejoin server",
+        Description = "",
+        Callback = function()
+            local ts = game:GetService("TeleportService")
+    
+            local p = game:GetService("Players").LocalPlayer
+            
+             
+            
+            ts:Teleport(game.PlaceId, p)
+        end
+    })
+    
+    Tabs.Settings:AddButton({
+        Title = "Hop Server",
+        Description = "",
+        Callback = function()
+            local Http = game:GetService("HttpService")
+            local TPS = game:GetService("TeleportService")
+            local Api = "https://games.roblox.com/v1/games/"
+            
+            local _place = game.PlaceId
+            local _servers = Api.._place.."/servers/Public?sortOrder=Asc&limit=100"
+            function ListServers(cursor)
+               local Raw = game:HttpGet(_servers .. ((cursor and "&cursor="..cursor) or ""))
+               return Http:JSONDecode(Raw)
+            end
+            
+            local Server, Next; repeat
+               local Servers = ListServers(Next)
+               Server = Servers.data[1]
+               Next = Servers.nextPageCursor
+            until Server
+            
+            TPS:TeleportToPlaceInstance(_place,Server.id,game.Players.LocalPlayer)
+        end
+    })
+    
+    
+    
+    
+        
+
+-- Addons:
+-- SaveManager (Allows you to have a configuration system)
+-- InterfaceManager (Allows you to have a interface managment system)
+
+-- Hand the library over to our managers
+SaveManager:SetLibrary(Fluent)
+InterfaceManager:SetLibrary(Fluent)
+
+-- Ignore keys that are used by ThemeManager.
+-- (we dont want configs to save themes, do we?)
+SaveManager:IgnoreThemeSettings()
+
+-- You can add indexes of elements the save manager should ignore
+SaveManager:SetIgnoreIndexes({})
+
+-- use case for doing it this way:
+-- a script hub could have themes in a global folder
+-- and game configs in a separate folder per game
+InterfaceManager:SetFolder("FluentScriptHub")
+SaveManager:SetFolder("FluentScriptHub/specific-game")
+
+InterfaceManager:BuildInterfaceSection(Tabs.Settings)
+
+Window:SelectTab(1)
+
+Fluent:Notify({
+    Title = "Notification",
+    Content = "The script has been loading",
+    Duration = 5
+})
+
+-- You can use the SaveManager:LoadAutoloadConfig() to load a config
+-- which has been marked to be one that auto loads!
+SaveManager:LoadAutoloadConfig()
+
+
